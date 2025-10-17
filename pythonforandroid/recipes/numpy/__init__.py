af93ab87f9372f3e5e3852bab85ab05fdb3497a3
Maxime Barbier <maxime@maxime-barbier.fr>
2025-10-17 10:17:30 +0200

    fix numpy flag

--- pythonforandroid/recipes/numpy/__init__.py
+++ pythonforandroid/recipes/numpy/__init__.py
@@ -54,7 +54,7 @@ class NumpyRecipe(MesonRecipe):
     def get_hostrecipe_env(self, arch):
         env = super().get_hostrecipe_env(arch)
         env["RANLIB"] = shutil.which("ranlib")
-        env["LDFLAGS"] = env["LDFLAGS"] + " -lm"
+        env["LDFLAGS"] = env.get("LDFLAGS", "") + " -lm"
         return env
 
 

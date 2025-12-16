3afc183936b97a7239555d41f0a9f785220a0461
Maxime Barbier <maxime@maxime-barbier.fr>
2025-12-16 23:15:05 +0100

    change version to master

--- pythonforandroid/recipes/ava-common/__init__.py
+++ pythonforandroid/recipes/ava-common/__init__.py
@@ -3,7 +3,7 @@ from pythonforandroid.recipe import PyProjectRecipe
 
 class AvaCommonRecipe(PyProjectRecipe):
     site_packages_name = 'ava-common'
-    version = '1.0.0'
+    version = 'master'
     url = 'git+https://github.com/Krozark/Ava-common'
     depends = ['jsonschema']
 

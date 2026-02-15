e9b643c7a42e9625f05de0db671c23a5af9dd3c6
Maxime Barbier <maxime@maxime-barbier.fr>
2026-02-15 16:21:25 +0100

    update regex

--- pythonforandroid/recipes/regex/__init__.py
+++ pythonforandroid/recipes/regex/__init__.py
@@ -3,7 +3,7 @@ from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 
 class RegexRecipe(CompiledComponentsPythonRecipe):
     name = 'regex'
-    version = '2019.06.08'
+    version = '2026.1.15'
     url = 'https://pypi.python.org/packages/source/r/regex/regex-{version}.tar.gz'  # noqa
 
     depends = ['setuptools']

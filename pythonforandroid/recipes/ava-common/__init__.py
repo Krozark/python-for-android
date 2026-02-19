af458ec5a62f4c84a73f22db161f55d7de29382f
Maxime Barbier <maxime@maxime-barbier.fr>
2026-02-19 02:25:11 +0100

    bump ava-common to 1.3.0

--- pythonforandroid/recipes/ava-common/__init__.py
+++ pythonforandroid/recipes/ava-common/__init__.py
@@ -2,8 +2,8 @@ from pythonforandroid.recipe import PyProjectRecipe
 
 
 class AvaCommonRecipe(PyProjectRecipe):
-    version = 'master'
-    url = 'git+https://github.com/Krozark/Ava-common/'
+    version = '1.3.0'
+    url = 'git+https://github.com/Krozark/Ava-common'
     depends = ['setuptools']
 
 

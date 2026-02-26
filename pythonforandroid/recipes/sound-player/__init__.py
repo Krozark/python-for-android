7a1154fd605b3bf6a0c1fd67085dc795eb293140
Maxime Barbier <maxime@maxime-barbier.fr>
2026-02-26 01:43:30 +0100

    update version of sound_player

--- pythonforandroid/recipes/sound-player/__init__.py
+++ pythonforandroid/recipes/sound-player/__init__.py
@@ -3,7 +3,7 @@ from pythonforandroid.recipe import PyProjectRecipe
 
 class AvaCommonRecipe(PyProjectRecipe):
     name = 'sound-player'
-    version = '1.0.5'
+    version = '1.0.8'
     url = 'https://github.com/Krozark/sound-player/archive/{version}.tar.gz'
     depends = ['setuptools']
 

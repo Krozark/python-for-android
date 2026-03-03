47cc685a583054d6d91772e88672bb5fc91c783b
Maxime Barbier <maxime@maxime-barbier.fr>
2026-03-03 17:12:32 +0100

    add private repo

--- pythonforandroid/recipes/lemitizer/__init__.py
+++ pythonforandroid/recipes/lemitizer/__init__.py
@@ -4,7 +4,7 @@ from pythonforandroid.recipe import PyProjectRecipe
 class LemmatizerRecipe(PyProjectRecipe):
     name = 'lemmatizer'
     version = '0.2.1'
-    url = 'https://github.com/Krozark/lemmatizer/archive/{version}.tar.gz'
+    url = 'git+https://github.com/Krozark/lemmatizer/'
     depends = ['setuptools']
 
 
--- pythonforandroid/recipes/sound-player/__init__.py
+++ pythonforandroid/recipes/sound-player/__init__.py
@@ -4,7 +4,7 @@ from pythonforandroid.recipe import PyProjectRecipe
 class SoundPlayerRecipe(PyProjectRecipe):
     name = 'sound-player'
     version = '1.1.0'
-    url = 'https://github.com/Krozark/sound-player/archive/{version}.tar.gz'
+    url = 'git+https://github.com/Krozark/sound-player/'
     depends = ['setuptools']
 
 

e94c1c3e07c2d4ce8599f4b286e0b10add09c704
Maxime Barbier <maxime@maxime-barbier.fr>
2026-03-03 17:09:54 +0100

    add private repo

--- pythonforandroid/recipes/ava-common/__init__.py
+++ pythonforandroid/recipes/ava-common/__init__.py
@@ -4,7 +4,7 @@ from pythonforandroid.recipe import PyProjectRecipe
 class AvaCommonRecipe(PyProjectRecipe):
     name = 'ava-common'
     version = '1.4.1'
-    url = 'https://github.com/Krozark/Ava-common/archive/{version}.tar.gz'
+    url = 'git+https://github.com/Krozark/Ava-common/archive/{version}.tar.gz'
     depends = ['setuptools']
 
 

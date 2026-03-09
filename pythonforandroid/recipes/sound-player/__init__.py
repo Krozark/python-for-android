e6c6f859ef8fb5b87ccf128bd494b6e8bae461e6
Maxime Barbier <maxime@maxime-barbier.fr>
2026-03-09 18:29:42 +0100

    soundplayer to 1.1.5

--- pythonforandroid/recipes/sound-player/__init__.py
+++ pythonforandroid/recipes/sound-player/__init__.py
@@ -5,7 +5,7 @@ from pythonforandroid.recipe import PyProjectRecipe
 
 class SoundPlayerRecipe(PyProjectRecipe):
     name = 'sound-player'
-    version = "1.1.4"
+    version = "1.1.5"
     # version = "master"
     url = 'git+https://github.com/Krozark/sound-player/'
     depends = ['setuptools']

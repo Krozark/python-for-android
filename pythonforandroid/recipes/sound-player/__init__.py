28b88cc0e919098775edce481894619350bbccd0
Maxime Barbier <maxime@maxime-barbier.fr>
2026-03-11 16:45:46 +0100

    put sound_player to master branch

--- pythonforandroid/recipes/sound-player/__init__.py
+++ pythonforandroid/recipes/sound-player/__init__.py
@@ -5,8 +5,8 @@ from pythonforandroid.recipe import PyProjectRecipe
 
 class SoundPlayerRecipe(PyProjectRecipe):
     name = 'sound-player'
-    version = "1.1.6"
-    # version = "master"
+    # version = "1.1.6"
+    version = "master"
     url = 'git+https://github.com/Krozark/sound-player/'
     depends = ['setuptools']
 

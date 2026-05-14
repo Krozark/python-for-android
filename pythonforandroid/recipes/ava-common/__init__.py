a3c6ebf77366f060067620b8fb6935d564e7685b
Maxime Barbier <maxime@maxime-barbier.fr>
2026-05-14 13:41:02 +0200

    chore: bump ava-common recipe to 26.05.14, bump version to 26.05.14

--- pythonforandroid/recipes/ava-common/__init__.py
+++ pythonforandroid/recipes/ava-common/__init__.py
@@ -3,7 +3,7 @@ from pythonforandroid.recipe import PyProjectRecipe
 
 class AvaCommonRecipe(PyProjectRecipe):
     name = 'ava-common'
-    version = '1.4.4'
+    version = '26.05.14'
     # version = 'master'
     url = 'git+https://github.com/Krozark/Ava-common/'
     depends = []

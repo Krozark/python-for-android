e9aff1255c1caa2820ac93bf84cfc3a1973c601c
Maxime Barbier <maxime@maxime-barbier.fr>
2026-06-05 19:43:37 +0200

    fix version

--- pythonforandroid/recipes/pywizlight/__init__.py
+++ pythonforandroid/recipes/pywizlight/__init__.py
@@ -3,7 +3,7 @@ from pythonforandroid.recipe import PythonRecipe
 
 class PywizlightRecipe(PythonRecipe):
     name = "pywizlight"
-    version = "0.6.3"
+    version = "v0.6.3"
     url = 'https://github.com/sbidy/pywizlight/archive/{version}.tar.gz'
     depends = ["click",]
     call_hostpython_via_targetpython = False

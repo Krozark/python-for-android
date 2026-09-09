e7c6ea7698ad60e23ad0aafd4193626748e129c8
Maxime Barbier <maxime@maxime-barbier.fr>
2026-09-09 18:13:11 +0200

    upgrade ava-commom

--- pythonforandroid/recipes/ava-common/__init__.py
+++ pythonforandroid/recipes/ava-common/__init__.py
@@ -3,7 +3,7 @@ from pythonforandroid.recipe import PyProjectRecipe
 
 class AvaCommonRecipe(PyProjectRecipe):
     name = 'ava-common'
-    version = '26.06.04'
+    version = '26.09.09'
     # version = 'master'
     url = 'git+https://github.com/Krozark/Ava-common/'
     depends = []
